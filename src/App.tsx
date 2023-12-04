d11a11a8785c7c87ff53de2953eec7504321a16f
requinDr <75610214+requinDr@users.noreply.github.com>
2023-12-04 21:14:43 +0100

    change styles for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AnimatedRoutes from './AnimatedRoutes';
-import { StrictMode } from 'react';
 
 function App() {
 
   return (
-    <StrictMode>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <AnimatedRoutes />
-      </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <AnimatedRoutes />
+    </BrowserRouter>
   )
 }
 
