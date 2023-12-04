3407da9d831e527245d68210ac60048238aad627
requinDr <75610214+requinDr@users.noreply.github.com>
2023-12-04 18:42:44 +0100

    no scrollbar visible on touch screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
