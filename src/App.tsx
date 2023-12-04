68107181902d997813190ff81a74c3589f926f25
requinDr <75610214+requinDr@users.noreply.github.com>
2023-12-04 21:14:43 +0100

    change styles for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AnimatedRoutes from './AnimatedRoutes';
-import { StrictMode } from 'react';
-import { useObserved } from "./utils/Observer";
-import strings from "./utils/lang";
 
 function App() {
   const [locale] = useObserved(strings, 'locale')
   document.documentElement.setAttribute('lang', locale)
 
   return (
-    <StrictMode>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <AnimatedRoutes />
-      </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <AnimatedRoutes />
+    </BrowserRouter>
   )
 }
 
