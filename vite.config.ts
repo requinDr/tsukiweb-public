9409a966846afaff8440f11b40276d55ecd3ae40
requinDr <75610214+requinDr@users.noreply.github.com>
2023-12-15 13:22:20 +0100

    moving project to top-level

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
-const targetFolder = "tsukiweb"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,11 +13,10 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
-  base: process.env.NODE_ENV === 'production' ? `/${targetFolder}/` : '/',
   server: {
     proxy: {
       '/static': {
-        target: `https://holofield.fr/${targetFolder}/static`,
+        target: `https://tsukiweb.holofield.fr/static`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/static/, '')
       }
