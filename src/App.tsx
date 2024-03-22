5bdeda268dadd12a5e6a187964f8e54af5bf36ba
requinDr <75610214+requinDr@users.noreply.github.com>
2024-03-22 15:50:17 +0100

    Move locale setter inside ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import strings from "./utils/lang";
 import { ErrorBoundary } from "react-error-boundary";
 import PageCrash from "./screens/CrashScreen";
 
-function App() {
+const LocaleSetter = () => {
   const [locale] = useObserved(strings, 'locale')
   document.documentElement.setAttribute('lang', locale)
 
+  return null
+}
+
+function App() {
   return (
     <ErrorBoundary FallbackComponent={PageCrash}>
+      <LocaleSetter />
       <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AnimatedRoutes />
       </BrowserRouter>
