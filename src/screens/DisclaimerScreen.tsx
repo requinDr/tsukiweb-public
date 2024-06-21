73794ea0a6b41c19dc4a4badcb971ad64db38ea3
requinDr <75610214+requinDr@users.noreply.github.com>
2024-06-21 16:05:02 +0200

    Fix React 18.3.1 warning for spread key props

--- src/screens/DisclaimerScreen.tsx
+++ src/screens/DisclaimerScreen.tsx
@@ -7,34 +7,35 @@ import { bb } from '../utils/Bbcode'
 import { useLanguageRefresh } from '../components/hooks/useLanguageRefresh'
 
 const DisclaimerScreen = () => {
-  const navigate = useNavigate()
-  useLanguageRefresh()
+	const navigate = useNavigate()
+	useLanguageRefresh()
 
-  useEffect(()=> {
-    const timeout = setTimeout(()=> {
-      sawDisclaimer()
-    }, 4000)
-    return ()=> clearTimeout(timeout)
-  }, [])
+	useEffect(()=> {
+		const timeout = setTimeout(()=> {
+			sawDisclaimer()
+		}, 4000)
+		return ()=> clearTimeout(timeout)
+	}, [])
 
-  const sawDisclaimer = () => {
-    navigate("/title")
-  }
+	const sawDisclaimer = () => {
+		navigate("/title")
+	}
 
-  return (
-    <motion.div
-      className="page" id="disclaimer"
-      initial={{opacity: 0}}
-      animate={{opacity: 1}}
-      exit={{opacity: 0, transition: {duration: 1}}}
-      onClick={sawDisclaimer}
-      >
-
-      <div className="box">
-        {strings.disclaimer.map((txt,i)=><p key={i}>{bb(txt)}</p>)}
-      </div>
-    </motion.div>
-  )
+	return (
+		<motion.div
+			className="page" id="disclaimer"
+			initial={{opacity: 0}}
+			animate={{opacity: 1}}
+			exit={{opacity: 0, transition: {duration: 1}}}
+			onClick={sawDisclaimer}
+		>
+			<div className="box">
+				{strings.disclaimer.map((txt, i)=>
+					<p key={i}>{bb(txt)}</p>
+				)}
+			</div>
+		</motion.div>
+	)
 }
 
 export default DisclaimerScreen
