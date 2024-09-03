3bda62f0d63d0456e62bb28c16003aeff079fb06
requinDr <75610214+requinDr@users.noreply.github.com>
2024-09-03 19:51:39 +0200

    increase bad endings size

--- src/components/endings/Oshiete.tsx
+++ src/components/endings/Oshiete.tsx
@@ -11,25 +11,46 @@ type Props = {
 }
 
 const Oshiete = ({unlocked, ending}: Props) => {
+
 	return (
 		<div className={classNames("badending", {"seen": unlocked})}>
 			{unlocked && ending ?
 				<img
 					src={chalkboard}
-					alt={`Bad Ending ${ending.scene}`}
+					alt={`Chalkboard Bad Ending ${ending.scene}`}
 					draggable={false}
 					onClick={() => playScene(ending.scene, {continueScript: false })}
 					data-tooltip-id="osiete"
-					data-tooltip-html={ReactDOMServer.renderToStaticMarkup(
-					<div>
-						{ending.name && <>{noBb(ending.name)}<br /></>}
-						{ending.day && <>Day: {ending.day}<br /></>}
-						{ending.scene}
-					</div>)} />
-			: <img src={chalkboard} alt="Bad Ending" draggable={false} />
+					data-tooltip-html={
+						ReactDOMServer.renderToString(
+							<Tooltip ending={ending} />
+						)
+					}
+				/>
+			:
+				<img
+					src={chalkboard}
+					alt="Bad Ending"
+					draggable={false}
+				/>
 			}
 		</div>
 	)
 }
 
 export default Oshiete
+
+type TooltipProps = {
+	ending: typeof osiete[keyof typeof osiete]
+}
+const Tooltip = ({ending}: TooltipProps) => {
+	if (!ending) return null
+
+	return (
+		<div>
+			{ending.name && <>{noBb(ending.name)}<br /></>}
+			{ending.day && <>Day: {ending.day}<br /></>}
+			{ending.scene}
+		</div>
+	)
+}
