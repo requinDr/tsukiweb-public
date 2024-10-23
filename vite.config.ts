7ece0783a2e44ca59c348ef274d7d3c037f4e48e
requinDr <75610214+requinDr@users.noreply.github.com>
2024-10-23 20:10:36 +0200

    deps update

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,28 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    svgrPlugin()
-  ],
-  define: {
-    'process.env': {}
-  },
-  server: {
-    proxy: {
-      '/static': {
-        target: `https://tsukidev.holofield.fr/static`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/static/, '')
-      }
-    }
-  }
+	plugins: [
+		react(),
+		viteTsconfigPaths(),
+		svgrPlugin()
+	],
+	define: {
+		'process.env': {}
+	},
+	server: {
+		proxy: {
+			'/static': {
+				target: `https://tsukidev.holofield.fr/static`,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/static/, '')
+			}
+		}
+	},
+	css :{
+		preprocessorOptions : {
+			scss: {
+				api: "modern",
+			}        
+		} 
+	}
 });
