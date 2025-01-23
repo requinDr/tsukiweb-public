a4ba90bd540c4bd712fdfb6b6924b68d8e6bee0d
requinDr <75610214+requinDr@users.noreply.github.com>
2025-01-23 19:42:32 +0100

    Simplify useLanguageRefresh

--- src/components/hooks/useLanguageRefresh.ts
+++ src/components/hooks/useLanguageRefresh.ts
@@ -1,23 +1,13 @@
-import { useReducer, useState } from "react";
-import { strings } from "../../translation/lang";
-import { observe, unobserve, useObserver } from "@tsukiweb-common/utils/Observer";
+import { useReducer } from "react"
+import { strings } from "../../translation/lang"
+import { useObserver } from "@tsukiweb-common/utils/Observer"
 
 /**
  * To use in components.
  * Forces a refresh of the component when the language is loaded.
  */
 export function useLanguageRefresh(once: boolean = false) {
-	if (once) {
-		const [_updated, setUpdated] = useState(false);
-		if (!_updated) {
-			const updater = ()=> {
-				setUpdated(true);
-				unobserve(strings, 'id', updater)
-			}
-			observe(strings, 'id', updater)
-		}
-	} else {
-		const [_updateNum, forceUpdate] = useReducer(x => (x + 1) % 100, 0);
-		useObserver(forceUpdate, strings, 'id')
-	}
+	const [, forceUpdate] = useReducer(x => x + 1, 0)
+
+	useObserver(forceUpdate, strings, 'id', { once })
 }
