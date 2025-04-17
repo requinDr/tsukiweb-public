882f173e905ddad37af719ea606247f21bce4958
loicfrance <loicfrance.developer@gmail.com>
2025-04-17 20:06:00 +0200

    Restore scene play in flowchart screen

--- src/screens/FlowchartScreen.tsx
+++ src/screens/FlowchartScreen.tsx
@@ -3,21 +3,22 @@ import { SCREEN, displayMode } from "../utils/display"
 import { useLanguageRefresh } from "../components/hooks/useLanguageRefresh"
 import { useScreenAutoNavigate } from "../components/hooks/useScreenAutoNavigate"
 import Flowchart from "components/flowchart/Flowchart"
+import { TsukihimeSceneName } from 'types'
+import { playScene } from 'utils/savestates'
 
 const FlowchartScreen = () => {
 	useScreenAutoNavigate(SCREEN.SCENES)
 	useLanguageRefresh()
 
-	function back(sceneLoaded: boolean) {
-		if (!sceneLoaded)
-			displayMode.screen = SCREEN.TITLE
+	const onSceneClick = (id: TsukihimeSceneName)=> {
+		playScene(id, {continueScript: true, viewedOnly: true})
 	}
 	return (
 		<div
 			className="page" id="scenes">
 			<div className="page">
 				<div className="page-content flowchart">
-					<Flowchart back={back} />
+					<Flowchart onSceneClick={onSceneClick} />
 				</div>
 			</div>
 		</div>
