82e5aad589f160c3bf7725ab646a67d3687e0cf6
requinDr <75610214+requinDr@users.noreply.github.com>
2025-05-16 20:27:14 +0200

    temp fix for the flowchart

--- src/components/flowchart/Flowchart.tsx
+++ src/components/flowchart/Flowchart.tsx
@@ -94,7 +94,9 @@ const Flowchart = ({flowchart, onSceneClick}: Props)=> {
 			{SVG_DEFS}
 			{[...flowchart.listNodes().filter(node=>node.visible).map(node=>
 				<Fragment key={node.id}>
-					{[...node.parents.filter(parent=>parent.state > FcNodeState.UNSEEN).map(
+					{[...node.parents
+						// .filter(parent=>parent.state > FcNodeState.UNSEEN)
+						.map(
 						parent => connectionPath(parent, node))]}
 					{node.scene &&
 						<SceneRenderer node={node} onClick={onSceneClick}/>}
