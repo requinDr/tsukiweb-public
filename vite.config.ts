aefce5f3838707e83a1cc2d9c79c715ade70b251
requinDr <75610214+requinDr@users.noreply.github.com>
2025-09-22 13:16:48 +0200

    Bypass proxy for certain files
    Use local json config instead of server ones that don't always match

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
+// List assets files to use from local instead of fetching from remote
+const regexPatterns = globToRegex([
+	'languages.json',
+	'game.json',
+	'lang.json',
+	// 'scenes/*.txt',
+])
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -18,8 +26,24 @@ export default defineConfig({
 			'/static': {
 				target: `https://tsukidev.holofield.fr/static`,
 				changeOrigin: true,
+				bypass: (req, res, options) => {
+					if (req.url && regexPatterns.some(r => r.test(req.url!))) {
+						// console.log(`Bypass proxy: ${req.url}`)
+						return req.url
+					}
+				},
 				rewrite: (path) => path.replace(/^\/static/, '')
 			}
 		}
 	}
-});
+})
+
+
+function globToRegex(patterns: string[]): RegExp[] {
+	return patterns.map(pattern => {
+		const escaped = pattern
+			.replace(/[.+^${}()|[\]\\]/g, '\\$&')
+			.replace(/\*/g, '.*')
+		return new RegExp(`.*/${escaped}(\\?.*)?$`)
+	})
+}
