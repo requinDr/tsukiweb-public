2f3bd1b36c1115884656593c10db96e75e2df35e
requinDr <75610214+requinDr@users.noreply.github.com>
2025-12-09 18:50:25 +0100

    Fix convert-images

--- tools/convert-images/index.js
+++ tools/convert-images/index.js
@@ -1,22 +1,14 @@
 import path from 'path'
-import { fileURLToPath } from 'url'
-import { processImages } from '../../tsukiweb-common/tools/processor.js'
+import { processImages } from '../../tsukiweb-common/tools/convert-images/processor.js'
 
 // Put in the input folder bg, event and tachi folders.
 // Put in the input_x2 folder bg, event and tachi folders (upscaled).
 // Generate images_thumb and images directories.
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const outputDir = "../../public/static/jp/"
 
-const inputDir = path.join(__dirname, 'input')
-const inputDirtX2 = path.join(__dirname, 'input_x2')
-const outputDir = path.join(__dirname, 'output')
-
+const thumbInputDir = 'input'
 const thumbOutputDir = path.join(outputDir, 'images_thumb')
-const imageX2OutputDir = path.join(outputDir, 'images')
-
-
 const thumbOptions = {
   resize: {
     width: 200,
@@ -30,6 +22,8 @@ const thumbOptions = {
   }
 }
 
+const inputDirX2 = 'input_x2'
+const outputDirX2 = path.join(outputDir, 'images')
 const imageX2Options = {
   avif: {
     quality: 60,
@@ -44,11 +38,8 @@ async function main() {
   try {
     console.log('--- Starting image conversion ---\n')
 
-    const conversionPromises = [
-      processImages(inputDir, thumbOutputDir, thumbOptions),
-      processImages(inputDirtX2, imageX2OutputDir, imageX2Options)
-    ]
-    await Promise.all(conversionPromises)
+    await processImages(thumbInputDir, thumbOutputDir, thumbOptions)
+    await processImages(inputDirX2, outputDirX2, imageX2Options)
 
     console.log('\n--- Image conversion finished ---')
   } catch (error) {
