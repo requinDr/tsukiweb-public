975231b298754ca62f92443ac545a5b57f148299
requinDr <75610214+requinDr@users.noreply.github.com>
2026-01-17 17:52:58 +0100

    Update react-error-boundary

--- src/screens/CrashScreen.tsx
+++ src/screens/CrashScreen.tsx
@@ -1,4 +1,5 @@
 import "@styles/crash.scss"
+import { getErrorMessage } from "react-error-boundary"
 import { APP_VERSION } from "utils/constants"
 
 
@@ -8,9 +9,22 @@ import { APP_VERSION } from "utils/constants"
  * - Should use as few dependencies as possible
  */
 type Props = {
-	error: Error
+	error: unknown
 }
 const PageCrash = ({ error }: Props) => {
+	if (!(error instanceof Error)) {
+		return (
+			<div id="page-crash" className="page">
+				<div className="page-content">
+					<h1>{getErrorMessage(error)}</h1>
+					<a href={import.meta.env.BASE_URL}>
+						Go back to the home page
+					</a>
+				</div>
+			</div>
+		)
+	}
+
 	return (
 		<div id="page-crash" className="page">
 			<div className="page-content">
@@ -21,10 +35,10 @@ const PageCrash = ({ error }: Props) => {
 				</p>
 				<details className="code">
 					<summary>
-						{error.message} <i className="ver">—v{APP_VERSION}</i>
+						{error?.message} <i className="ver">—v{APP_VERSION}</i>
 					</summary>
 					<p>
-						{error.stack}
+						{error?.stack}
 					</p>
 				</details>
 				<a href={import.meta.env.BASE_URL}>
