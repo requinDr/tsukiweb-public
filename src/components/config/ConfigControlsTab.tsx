4cade788e058e5ab50661b416a22745b0b34f18d
requinDr <75610214+requinDr@users.noreply.github.com>
2026-04-03 22:44:41 +0200

    Style consistency and better spacing on Config

--- src/components/config/ConfigControlsTab.tsx
+++ src/components/config/ConfigControlsTab.tsx
@@ -27,27 +27,29 @@ const ConfigControlsTab = () => {
 	return (
 		<PageSection>
 			{keymap.current.map(([action, keys], i)=> 
-				<div key={i} className="keyMap">
-					<div className="action">
+				<div key={i} className="key-map">
+					<div className="config-name">
 						{bb(controlStrings[action])}
 					</div>
 
-					{keys.map(({code, key, ctrlKey, altKey, shiftKey, repeat})=>
-						<kbd key={`${code || key}`} className="keyItem">
-							{ctrlKey ? "Ctrl + " : ""}
-							{altKey ? "Alt + " : ""}
-							{shiftKey ? "Shift + " : ""}
-							{code || key}
-							{repeat != undefined && (
-								repeat && <>
-									&nbsp;
-									<span className="info">
-										{bb(controlStrings["_hold"])}
-									</span>
-								</>
-							)}
-						</kbd>
-					)}
+					<div className="config-actions">
+						{keys.map(({code, key, ctrlKey, altKey, shiftKey, repeat})=>
+							<kbd key={`${code || key}`} className="key-item">
+								{ctrlKey ? "Ctrl + " : ""}
+								{altKey ? "Alt + " : ""}
+								{shiftKey ? "Shift + " : ""}
+								{code || key}
+								{repeat != undefined && (
+									repeat && <>
+										&nbsp;
+										<span className="info">
+											{bb(controlStrings["_hold"])}
+										</span>
+									</>
+								)}
+							</kbd>
+						)}
+					</div>
 				</div>
 			)}
 		</PageSection>
