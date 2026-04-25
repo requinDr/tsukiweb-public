94b84addedeabeb302d24b3add7c7d136e770af1
requinDr <75610214+requinDr@users.noreply.github.com>
2026-04-25 23:41:29 +0200

    Calculate badges once

--- src/components/flowchart/badges.tsx
+++ src/components/flowchart/badges.tsx
@@ -1,118 +1,123 @@
 import { FcNode } from "utils/flowchart"
 import { COLUMN_WIDTH, DY } from "@tsukiweb-common/flowchart"
 import { SCENE_ATTRS } from "utils/constants"
-import { SceneName } from "types"
+
 const HEART_OFFSET = `0,2.5`
 const commonProps = {
-    fill:"none",
-    stroke:"white",
-    strokeWidth:"0.15mm",
-    strokeLinecap:"round",
-    strokeLinejoin:"round"
+  fill:"none",
+  stroke:"white",
+  strokeWidth:"0.15mm",
+  strokeLinecap:"round",
+  strokeLinejoin:"round"
 } as const
 
 export const BADGES_DEFINES = <defs>
-    <linearGradient id="ark_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="color-mix(in oklch, var(--route-ark) 80%, white)" />
-        <stop offset="1" stopColor="color-mix(in oklch, var(--route-ark) 80%, black)" />
-    </linearGradient>
-    <linearGradient id="cel_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="color-mix(in oklch, var(--route-cel) 80%, white)" />
-        <stop offset="1" stopColor="color-mix(in oklch, var(--route-cel) 80%, black)" />
-    </linearGradient>
-    <linearGradient id="aki_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="color-mix(in oklch, var(--route-aki) 80%, white)" />
-        <stop offset="1" stopColor="color-mix(in oklch, var(--route-aki) 80%, black)" />
-    </linearGradient>
-    <linearGradient id="his_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="color-mix(in oklch, var(--route-his) 80%, white)" />
-        <stop offset="1" stopColor="color-mix(in oklch, var(--route-his) 80%, black)" />
-    </linearGradient>
-    <linearGradient id="koha_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="color-mix(in oklch, var(--route-koha) 80%, white)" />
-        <stop offset="1" stopColor="color-mix(in oklch, var(--route-koha) 80%, black)" />
-    </linearGradient>
-    <linearGradient id="len_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="var(--color-ark-1, #242424)" />
-        <stop offset="1" stopColor="var(--color-ark-2, #606060)" />
-    </linearGradient>
-    <linearGradient id="akira_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="var(--color-cel-1, #664c73)" />
-        <stop offset="1" stopColor="var(--color-cel-2, #be59f0)" />
-        <stop offset="1" stopColor="var(--color-cel-2, #ba5ce9)" />
-    </linearGradient>
-    <linearGradient id="nana_grad" x1="0" x2="1" y1="1" y2="0">
-        <stop offset="0" stopColor="var(--color-aki-1, #1f3a65)" />
-        <stop offset="1" stopColor="var(--color-aki-2, #3564af)" />
-    </linearGradient>
-    <path id="regard_+" d="m1.75,0.75l0,1.75m-0.875,-0.875l1.75,0" stroke="var(--regard-plus-color, #00ff00)" fill="none"/>
-    <path id="regard_-" d="m1.75,0.85m-0.875,0.5l1.75,0" stroke="var(--regard-minus-color, #ff0000)" fill="none"/>
-    {[1, 2, 3].map(n=>
-        <g key={`regard_${n}`} id={`regard_${n}`}>
-            <path d={`m${HEART_OFFSET}l-2.5,-2.5a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2
-                ${"m-5,-2.2a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2".repeat(n-1)}`}/>
-            <use href="#regard_+"/>
-        </g>
-    )}
-    {[-4, -1].map(n=>
-        <g key={`regard_${n}`} id={`regard_${n}`}>
-            <path d={`m${HEART_OFFSET}l-2.5,-2.5a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2
-                m-2.5,0.5l0.6,-0.5l-1.3,-1l0.7,-1.2
-                ${`m-2.5,0a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2
-                m-2.29,-0.3l-0.91,-0.7l0.7,-1.2`.repeat(-n-1)}`}/>
-            <use href="#regard_-"/>
-        </g>
-    )}
-    <polygon id="flag-icon" points="0,-3.14 3,-1 1.85,2.53 -1.85,2.53 -3,-1" fill="#668a00" />
-    <g id="sel-icon">
-        <polygon points="0,-3 3,0 0,3 -3,0" fill="lime"/>
-        <text fontFamily="sans-serif" fontSize="4" stroke="none" fill="white" alignmentBaseline="central" textAnchor="middle">
-        ?
-        </text>
+  <linearGradient id="ark_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="color-mix(in oklch, var(--route-ark) 80%, white)" />
+    <stop offset="1" stopColor="color-mix(in oklch, var(--route-ark) 80%, black)" />
+  </linearGradient>
+  <linearGradient id="cel_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="color-mix(in oklch, var(--route-cel) 80%, white)" />
+    <stop offset="1" stopColor="color-mix(in oklch, var(--route-cel) 80%, black)" />
+  </linearGradient>
+  <linearGradient id="aki_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="color-mix(in oklch, var(--route-aki) 80%, white)" />
+    <stop offset="1" stopColor="color-mix(in oklch, var(--route-aki) 80%, black)" />
+  </linearGradient>
+  <linearGradient id="his_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="color-mix(in oklch, var(--route-his) 80%, white)" />
+    <stop offset="1" stopColor="color-mix(in oklch, var(--route-his) 80%, black)" />
+  </linearGradient>
+  <linearGradient id="koha_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="color-mix(in oklch, var(--route-koha) 80%, white)" />
+    <stop offset="1" stopColor="color-mix(in oklch, var(--route-koha) 80%, black)" />
+  </linearGradient>
+  <linearGradient id="len_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="var(--color-ark-1, #242424)" />
+    <stop offset="1" stopColor="var(--color-ark-2, #606060)" />
+  </linearGradient>
+  <linearGradient id="akira_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="var(--color-cel-1, #664c73)" />
+    <stop offset="1" stopColor="var(--color-cel-2, #be59f0)" />
+    <stop offset="1" stopColor="var(--color-cel-2, #ba5ce9)" />
+  </linearGradient>
+  <linearGradient id="nana_grad" x1="0" x2="1" y1="1" y2="0">
+    <stop offset="0" stopColor="var(--color-aki-1, #1f3a65)" />
+    <stop offset="1" stopColor="var(--color-aki-2, #3564af)" />
+  </linearGradient>
+  <path id="regard_+" d="m1.75,0.75l0,1.75m-0.875,-0.875l1.75,0" stroke="var(--regard-plus-color, #00ff00)" fill="none"/>
+  <path id="regard_-" d="m1.75,0.85m-0.875,0.5l1.75,0" stroke="var(--regard-minus-color, #ff0000)" fill="none"/>
+  {[1, 2, 3].map(n=>
+    <g key={`regard_${n}`} id={`regard_${n}`}>
+      <path d={`m${HEART_OFFSET}l-2.5,-2.5a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2
+        ${"m-5,-2.2a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2".repeat(n-1)}`}/>
+      <use href="#regard_+"/>
     </g>
-    <g id="cond-icon">
-        <polygon points="0,-3 3,0 0,3 -3,0" fill="lime"/>
-        <path d="m 2.5,1.5 l-2.5,2.5 -2.5,-2.5" strokeWidth="0.525mm"/>
-        <path d="m 2.5,1.5 l-2.5,2.5 -2.5,-2.5 m 2.5,2.5 l 0,-2" stroke="lime"/>
+  )}
+  {[-4, -1].map(n=>
+    <g key={`regard_${n}`} id={`regard_${n}`}>
+      <path d={`m${HEART_OFFSET}l-2.5,-2.5a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2
+        m-2.5,0.5l0.6,-0.5l-1.3,-1l0.7,-1.2
+        ${`m-2.5,0a1.66,1.66,0,1,1,2.5,-2.2a1.66,1.66,0,1,1,2.5,2.2
+        m-2.29,-0.3l-0.91,-0.7l0.7,-1.2`.repeat(-n-1)}`}/>
+      <use href="#regard_-"/>
     </g>
+  )}
+  <polygon id="flag-icon" points="0,-3.14 3,-1 1.85,2.53 -1.85,2.53 -3,-1" fill="#668a00" />
+  <g id="sel-icon">
+    <polygon points="0,-3 3,0 0,3 -3,0" fill="lime"/>
+    <text fontFamily="sans-serif" fontSize="4" stroke="none" fill="white" alignmentBaseline="central" textAnchor="middle">
+    ?
+    </text>
+  </g>
+  <g id="cond-icon">
+    <polygon points="0,-3 3,0 0,3 -3,0" fill="lime"/>
+    <path d="m 2.5,1.5 l-2.5,2.5 -2.5,-2.5" strokeWidth="0.525mm"/>
+    <path d="m 2.5,1.5 l-2.5,2.5 -2.5,-2.5 m 2.5,2.5 l 0,-2" stroke="lime"/>
+  </g>
 </defs>
 
+
 type SceneBadgesProps = {
-    node: FcNode
+  node: FcNode
 }
 
-export function getNodeBadges(node: FcNode) {
-    const [char, regard_entry] = Object.entries(SCENE_ATTRS.badges.regards).find(
-        ([char, entries])=> Object.keys(entries).includes(node.id)) ?? [null, null]
-    const flag = Object.entries(SCENE_ATTRS.badges.flags).find(
-        ([flg, ids])=> ids.includes(node.id as SceneName))?.[0]
-    
-    return [
-        flag,
-        char ? [char, regard_entry[node.id as SceneName]]
-             : [null, null]
-    ] as const
-}
+type BadgeEntry = { flag?: string; char?: string; value?: number }
+const BADGE_MAP = new Map<string, BadgeEntry>()
+
+for (const [char, entries] of Object.entries(SCENE_ATTRS.badges.regards))
+  for (const [id, value] of Object.entries(entries))
+    BADGE_MAP.set(id, { ...BADGE_MAP.get(id), char, value: value as number })
+
+for (const [flag, ids] of Object.entries(SCENE_ATTRS.badges.flags))
+  for (const id of ids)
+    BADGE_MAP.set(id, { ...BADGE_MAP.get(id), flag })
+
+export const getNodeBadges = (id: string) => BADGE_MAP.get(id)
+
 export const SceneBadges = ({node}: SceneBadgesProps)=> {
-    const [flag, [char, value]] = getNodeBadges(node)
-    let regard_badge = undefined, flag_badge = undefined
-    if (char) {
-        const dx = node.right - (node.width > 0 ? COLUMN_WIDTH - node.width : 0)
-        const dy = node.bottom - (node.height > 0 ? DY/2 : 0)
-        regard_badge = <use href={`#regard_${value}`}
-                            fill={`url(#${char}_grad`}
-                            transform={`translate(${dx}, ${dy})`} />
-    }
-    if (flag) {
-        const dx = node.left + (node.width > 0 ? COLUMN_WIDTH - node.width : 0)
-        const dy = node.bottom - (node.height > 0 ? DY/2 : 0)
-        flag_badge = <g transform={`translate(${dx}, ${dy})`}>
-            <use href="#flag-icon"/>
-            <text y="1.6" font-family="sans-serif" fontSize="4" stroke="none" fill="white" text-anchor="middle">
-            {flag}
-            </text>
-        </g>
-    }
-    return <g {...commonProps}>{regard_badge}{flag_badge}</g>
-
+  const badge = getNodeBadges(node.id)
+  if (!badge) return null
+  const { flag, char, value } = badge
+  
+  let regard_badge = undefined, flag_badge = undefined
+  if (char) {
+    const dx = node.right - (node.width > 0 ? COLUMN_WIDTH - node.width : 0)
+    const dy = node.bottom - (node.height > 0 ? DY/2 : 0)
+    regard_badge = <use href={`#regard_${value}`}
+              fill={`url(#${char}_grad`}
+              transform={`translate(${dx}, ${dy})`} />
+  }
+  if (flag) {
+    const dx = node.left + (node.width > 0 ? COLUMN_WIDTH - node.width : 0)
+    const dy = node.bottom - (node.height > 0 ? DY/2 : 0)
+    flag_badge = <g transform={`translate(${dx}, ${dy})`}>
+      <use href="#flag-icon"/>
+      <text y="1.6" fontFamily="sans-serif" fontSize="4" stroke="none" fill="white" textAnchor="middle">
+        {flag}
+      </text>
+    </g>
+  }
+  
+  return <g {...commonProps}>{regard_badge}{flag_badge}</g>
 }
