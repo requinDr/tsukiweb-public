833c5b08f6972e9715b4ca18863e52384153e1ec
requinDr <75610214+requinDr@users.noreply.github.com>
2026-04-27 22:59:50 +0200

    Simpler popover

--- src/components/flowchart/ScenePopover.tsx
+++ src/components/flowchart/ScenePopover.tsx
@@ -1,5 +1,5 @@
 import { GraphicsGroup } from "@tsukiweb-common/graphics"
-import { bb } from "@tsukiweb-common/utils/Bbcode"
+import { bb, noBb } from "@tsukiweb-common/utils/Bbcode"
 import { SceneName } from "types"
 import { FcNode, getSceneGraph } from "utils/flowchart"
 import { getNodeBadges } from "./badges"
@@ -17,13 +17,14 @@ const ScenePopover = ({ node }: PopoverProps) => {
 				{graph && <GraphicsGroup images={graph} />}
 			</div>
 			<div className="title">
-				{bb(node.displayName)}
+				{node.displayName.split('[br/]')
+					.map((line, i) => <div key={i}>{noBb(line)}</div>)}
 			</div>
 			<div className="id">
 				{bb(node.id)}
 			</div>
 			{badges &&
-				<svg className="badges" viewBox="-4 -4 50 8" preserveAspectRatio="xMinYMid meet">
+				<svg className="badges" viewBox="-50 -4 56 8" preserveAspectRatio="xMaxYMid meet">
 					{badges.flag &&
 						<g className="badge">
 							<use href="#flag-icon"/>
@@ -33,15 +34,17 @@ const ScenePopover = ({ node }: PopoverProps) => {
 						</g>
 					}
 					{badges.char &&
-						<g className="badge" transform={`translate(${badges.flag ? 4 : -4},0)`}>
-							<image href={`./chars/${badges.char}.webp`}
-									className="badge" x={1} y={-3.5} height={7}/>
-							{[...Array(Math.abs(badges.value)).fill(0)].map((_, i)=> 
-							<use key={i} href={`#regard_${Math.sign(badges.value)}`}
-								fill={`url(#${badges.char}_grad)`}
-								transform={`translate(${11+5*i},0) scale(0.75)`}
+						<g className="badge" transform={`translate(${badges.flag ? -4 : 4},0)`}>
+							{[...Array(Math.abs(badges.value)).fill(0)].map((_, i) =>
+								<use key={i} href={`#regard_${Math.sign(badges.value)}`}
+									fill={`url(#${badges.char}_grad)`}
+									transform={`translate(${-(Math.abs(badges.value) - i) * 5},0) scale(0.75)`}
 								/>
 							)}
+							<image href={`./chars/${badges.char}.webp`}
+								className="badge"
+								x={-(Math.abs(badges.value) * 5 + 8)}
+								y={-3.5} height={7} />
 						</g>
 					}
 				</svg>
