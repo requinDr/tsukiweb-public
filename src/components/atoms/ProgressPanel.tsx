442adfd23538090a5c10072787a248ce822fd943
loicfrance <loicfrance.developer@gmail.com>
2026-05-07 23:48:59 +0200

    Add missing progress panel component

---
+++ src/components/atoms/ProgressPanel.tsx
@@ -0,0 +1,68 @@
+import { PartialJSON } from "@tsukiweb-common/types"
+import { BADGES_DEFINES } from "components/flowchart/badges"
+import { BiSolidHeart, BiSolidStar } from "react-icons/bi"
+import { Regard, ScriptPlayer } from "script/ScriptPlayer"
+import { strings } from "translation/lang"
+import { CharId } from "types"
+import { CHARS } from "utils/constants"
+
+type Props = {
+    script: ScriptPlayer,
+    flags?: never
+    regard?: never
+} | {
+    script?: never,
+    flags: Set<string>|Array<string>,
+    regard: PartialJSON<Regard>
+}
+const AffectionRow = ({ char, value, max }: {char: CharId, value: number, max: number}) => {
+    return (
+        <div className="row">
+            <img className="char" src={`./chars/${char}.webp`}
+                 alt={strings.characters[char]} />
+            <div className="hearts">
+                {Array(max ? Math.min(value, max) : value)
+                    .fill(null).map((_, index) =>
+                    <BiSolidHeart key={`${char}-heart-${index}`}
+                        className="heart-icon"
+                        style={{ fill: `url(#${char}_grad)` }}
+                    />
+                )}
+            </div>
+        </div>
+    )
+}
+const FlagsList = ({ flags }: { flags: string[] }) => {
+	return (
+		<div className="flags-list">
+			{flags.map(flag =>
+				<svg key={flag} className="badge" viewBox="-4 -4 8 8" preserveAspectRatio="xMinYMid meet">
+					<g className="badge">
+						<use href="#flag-icon"/>
+						<text y="1.6" stroke="none" fill="white" textAnchor="middle">
+							{flag}
+						</text>
+					</g>
+				</svg>
+			)}
+		</div>
+	)
+}
+
+export const ProgressPanel = ({script, flags, regard}: Props) => {
+
+    if (script) {
+        flags = script.flags
+        regard = script.regard
+    }
+    return <div className="progress-panel">
+        <svg width="20" height="20" className="defs">{BADGES_DEFINES}</svg>
+        {script &&
+        CHARS.filter(c=>script.readVariable(`%clear_${c}`)).map(c=>
+            <BiSolidStar key={`${c}-heart-index`} className="heart-icon" style={{fill: `url(#${c}_grad)`}}/>
+        )}
+        {CHARS.filter(c=>regard![c] ?? 0 > 0).map(c=>
+            <AffectionRow char={c} value={regard![c]!} max={20}/>)}
+        <FlagsList flags={Array.from(flags!)}/>
+    </div>
+}
