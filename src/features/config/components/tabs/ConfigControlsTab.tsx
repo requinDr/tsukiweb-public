2aaf27525bf664ff6cb41ffbf016ddc0a8e1c989
requinDr <75610214+requinDr@users.noreply.github.com>
2026-08-31 18:02:06 +0200

    Improve Controls display, separate key press from gestures

--- src/features/config/components/tabs/ConfigControlsTab.tsx
+++ src/features/config/components/tabs/ConfigControlsTab.tsx
@@ -1,10 +1,10 @@
+import { Fragment } from "react"
 import { PageSection } from "@tsukiweb/common/ui-core"
 import { bb } from "@tsukiweb/common/utils/Bbcode"
 import { inGameControls, inGameGestures } from "features/game/utils/keybind"
 import { MdSwipeDown, MdSwipeLeft, MdSwipeRight, MdSwipeUp } from "react-icons/md";
 import { useStrings } from "translation/lang";
 
-const directionArrows = {up: <MdSwipeUp />, right: <MdSwipeLeft />, down: <MdSwipeDown />, left: <MdSwipeRight />}
 
 const ConfigControlsTab = () => {
 	const strings = useStrings()
@@ -13,47 +13,120 @@ const ConfigControlsTab = () => {
 		...Object.keys(inGameControls),
 		...inGameGestures.map(gesture => gesture.action),
 	])].filter(action => Object.hasOwn(controlStrings, action))
+
 	return (
 		<PageSection>
-			{actions.map(action => {
-				const keys = (inGameControls[action] ?? [])
-					.filter(({key, code}) => key || code)
-				const gestures = inGameGestures.filter(gesture => gesture.action === action)
-				return <div key={action} className="key-map">
+			{actions.map(action =>
+				<div key={action} className="key-map">
 					<div className="config-name">
 						{bb(controlStrings[action])}
 					</div>
 
 					<div className="config-actions">
-						{keys.map(({code, key, ctrlKey, altKey, shiftKey, repeat})=>
-							<kbd key={`${code || key}`} className="key">
-								{ctrlKey ? "Ctrl + " : ""}
-								{altKey ? "Alt + " : ""}
-								{shiftKey ? "Shift + " : ""}
-								{code || key}
-								{repeat != undefined && (
-									repeat && <>
-										<span className="info">
-											{controlStrings["_hold"]}
-										</span>
-									</>
-								)}
-							</kbd>
-						)}
-						{gestures.map(gesture => {
-							const layers = gesture.layers
-								.map(layer => controlStrings[`_layer-${layer}`])
-								.join(' / ')
-							return <kbd key={`${gesture.direction}-${layers}`} className="key">
-								{directionArrows[gesture.direction]}
-								<span className="info">({layers})</span>
-							</kbd>
-						})}
+						<KeyboardControls
+							action={action}
+							controlStrings={controlStrings}
+						/>
+
+						<GestureControls
+							action={action}
+							controlStrings={controlStrings}
+						/>
 					</div>
 				</div>
-			})}
+			)}
 		</PageSection>
 	)
 }
 
 export default ConfigControlsTab
+
+
+
+type ControlProps = {
+	action: string
+	controlStrings: Record<string, string>
+}
+
+const keyLabels: Record<string, string> = {
+	ArrowUp: '↑',
+	ArrowDown: '↓',
+	PageUp: '⇞',
+	PageDown: '⇟',
+	Control: 'Ctrl',
+	Meta: '⌘',
+}
+const KeyboardControls = ({ action, controlStrings }: ControlProps) => {
+	const keys = (inGameControls[action] ?? [])
+		.filter(({ key, code }) => key || code)
+
+	return (
+		<>
+			{keys.map(({ code, key, ctrlKey, altKey, shiftKey, repeat }) => {
+				const value = code || key!
+
+				const parts = [
+					ctrlKey && "Ctrl",
+					altKey && "Alt",
+					shiftKey && "Shift",
+					keyLabels[value] ?? value,
+				].filter(Boolean) as string[]
+
+				return (
+					<span
+						key={`${parts.join("+")}-${repeat}`}
+						className="shortcut"
+					>
+						{parts.map((part, partIndex) =>
+							<Fragment key={part}>
+								{partIndex > 0 && <span>+</span>}
+
+								<kbd className="key">{part}</kbd>
+
+								{partIndex === parts.length - 1 && repeat && (
+									<span className="info">
+										{controlStrings._hold}
+									</span>
+								)}
+							</Fragment>
+						)}
+					</span>
+				)
+			})}
+		</>
+	)
+}
+
+
+const directionArrows = {
+	up: <MdSwipeUp aria-label="Swipe up" />,
+	right: <MdSwipeRight aria-label="Swipe right" />,
+	down: <MdSwipeDown aria-label="Swipe down" />,
+	left: <MdSwipeLeft aria-label="Swipe left" />
+}
+const GestureControls = ({ action, controlStrings }: ControlProps) => {
+	const gestures = inGameGestures.filter(gesture => gesture.action === action)
+
+	return (
+		<>
+			{gestures.map(gesture => {
+				const layers = gesture.layers
+					.map(layer => controlStrings[`_layer-${layer}`])
+					.join(" / ")
+
+				return (
+					<span
+						key={`${gesture.direction}-${layers}`}
+						className="shortcut gesture"
+					>
+						<kbd className="key">
+							{directionArrows[gesture.direction]}
+						</kbd>
+
+						<span className="info">{layers}</span>
+					</span>
+				)
+			})}
+		</>
+	)
+}
